11405ad6d6a93a13eda2ccbac32b196cae79f594
Nathan Pahucki <npahucki@gmail.com>
2015-04-10 12:39:28 -0300

    Layout tweaks. Fix launch screen.

--- Bubli/IntroViewController.swift
+++ Bubli/IntroViewController.swift
@@ -15,6 +15,12 @@ class IntroViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var copyrightLabel: UILabel!
     @IBOutlet weak var moreAppsView: UIView!
+    @IBOutlet weak var titleToImageContainerConstraint: NSLayoutConstraint!
+    @IBOutlet weak var titleTopConstraint: NSLayoutConstraint!
+
+    @IBOutlet weak var containerHeightConstraint: NSLayoutConstraint!
+    
+    var didInitialAnimation : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,19 +28,61 @@ class IntroViewController: UIViewController {
         startButton.alpha = 0.0
         moreAppsView.alpha = 0.0
         
+        // 300, max needed to show 3 additional items. 
+        // 172 a little less than two items. 
+        // 480 is the 4s screen size.
+//        containerHeightConstraint.constant =  min(300, 206 + (UIScreen.mainScreen().bounds.size.height - 480))
         
-        UIView.animateWithDuration(1.0, delay: 1.0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
-            self.backImage.alpha = 0.05
-            self.copyrightLabel.alpha = 0.0
-            self.explainerLabel.alpha = 1.0
-            self.moreAppsView.alpha = 1.0
-            self.startButton.alpha = 1.0
-        }) { (Bool complete) -> Void in
-            //
-        }
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "initiateViewChangeAnimation", userInfo: nil, repeats: false)
+        
     }
     
     override func prefersStatusBarHidden() -> Bool {
         return true;
     }
+    
+    @objc func initiateViewChangeAnimation() {
+        
+        if UIScreen.mainScreen().bounds.size.height < 768 {
+            titleToImageContainerConstraint.active = false
+            self.view.setNeedsLayout()
+        }
+
+        UIView.animateWithDuration(0.5, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+            self.backImage.alpha = 0.05
+            self.copyrightLabel.alpha = 0.0
+            self.explainerLabel.alpha = 1.0
+            self.moreAppsView.alpha = 1.0
+            self.startButton.alpha = 1.0
+            self.view.layoutIfNeeded()
+            }) { (Bool complete) -> Void in
+                self.didInitialAnimation = true
+        }
+    }
+    
+//    override func viewWillTransitionToSize(size: CGSize,
+//        withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+//            super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+//            titleToImageContainerConstraint.active = size.height >= 768
+//            // 300, max needed to show 3 additional items.
+//            // 172 a little less than two items.
+//            // 480 is the 4s screen size.
+////            containerHeightConstraint.constant =  min(300, 206 + size.height - 480)
+//            self.view.setNeedsLayout()
+//    }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if didInitialAnimation {
+            let activte = UIScreen.mainScreen().bounds.size.height >= 768
+            if titleToImageContainerConstraint.active != activte {
+                    titleToImageContainerConstraint.active = activte
+                self.view.setNeedsLayout()
+                
+            }
+        }
+    }
+    
+    
 }
